01a6c176a0c872c2fe902fb202a68e31436d0f4e
Benjamin Gerber <benjamin.gerber@camptocamp.com>
2023-01-09 08:42:22 +0100

    Better test of icon-animation

--- examples/flicc-icon-animation.js
+++ examples/flicc-icon-animation.js
@@ -3,14 +3,16 @@ import Icon from '../src/ol/style/Icon.js';
 import Map from '../src/ol/Map.js';
 import Point from '../src/ol/geom/Point.js';
 import View from '../src/ol/View.js';
-import vectorLayer from '../src/ol/renderer/canvas/VectorLayer.js';
-import {Circle as CircleStyle, Stroke, Style} from '../src/ol/style.js';
-import {OSM, Vector as VectorSource} from '../src/ol/source.js';
+import {Circle, Fill, Style} from '../src/ol/style.js';
+import {
+  Cluster as ClusterSource,
+  OSM,
+  Vector as VectorSource,
+} from '../src/ol/source.js';
 import {Tile as TileLayer, Vector as VectorLayer} from '../src/ol/layer.js';
-import {easeIn, easeOut, inAndOut} from '../src/ol/easing.js';
 import {fromLonLat} from '../src/ol/proj.js';
 import {getVectorContext} from '../src/ol/render.js';
-import {unByKey} from '../src/ol/Observable.js';
+import {inAndOut} from '../src/ol/easing.js';
 
 const iconSize = 20;
 
@@ -20,22 +22,58 @@ const tileLayer = new TileLayer({
   }),
 });
 
-const source = new VectorSource({
+const vsource = new VectorSource({
   wrapX: false,
 });
-const vector = new VectorLayer({
-  source: source,
+
+const csource = new ClusterSource({
+  source: vsource,
+  wrapX: false,
+});
+
+const vectorX = new VectorLayer({
+  source: new VectorSource(),
   style: new Style({
-    image: new Icon({
-      crossOrigin: 'anonymous',
-      src: 'data/flicc-point.svg',
-      scale: 1,
+    image: new Circle({
+      radius: 0,
+      fill: new Fill({
+        color: 'blue',
+      }),
     }),
   }),
 });
+vectorX.getSource().addFeature(new Feature(new Point([0, 0])));
+
+const vector = new VectorLayer({
+  source: csource,
+  style: (features) => {
+    const isLast = features
+      .get('features')
+      .find((feature) => feature.get('last'));
+    if (!isLast) {
+      return new Style({
+        image: new Icon({
+          crossOrigin: 'anonymous',
+          src: 'data/flicc-point.svg',
+          scale: 1,
+          color: 'blue',
+        }),
+      });
+    } else {
+      return new Style({
+        image: new Icon({
+          crossOrigin: 'anonymous',
+          src: 'data/flicc-point.svg',
+          scale: 1,
+          opacity: 0.4,
+        }),
+      });
+    }
+  },
+});
 
 const map = new Map({
-  layers: [tileLayer, vector],
+  layers: [tileLayer, vector, vectorX],
   target: 'map',
   view: new View({
     center: [0, 0],
@@ -44,22 +82,12 @@ const map = new Map({
   }),
 });
 
-function addRandomFeature(last) {
-  const x = Math.random() * 360 - 180;
-  const y = Math.random() * 170 - 85;
-  const geom = new Point(fromLonLat([x, y]));
-  const feature = new Feature(geom);
-  if (last) {
-    feature.set('last', true);
-  }
-  source.addFeature(feature);
-}
-
 const duration = 750;
 function flash(feature) {
   const start = Date.now();
   const flashGeom = feature.getGeometry().clone();
-  const listenerKey = vector.on('postrender', animate);
+  // const listenerKey = vector.on('postrender', animate);
+  vectorX.on('postrender', animate);
 
   function animate(event) {
     const frameState = event.frameState;
@@ -77,8 +105,8 @@ function flash(feature) {
     // TODO: A cache could be great here.
     const style = new Style({
       image: new Icon({
-        src: 'data/flicc-point.svg',
         crossOrigin: 'anonymous',
+        src: 'data/flicc-point.svg',
         scale: (iconSize + iconSize * 0.7 * ratio) / iconSize,
       }),
     });
@@ -95,23 +123,47 @@ function flash(feature) {
  */
 const doFlash = () => {
   // on cluster change
-  const lastFeature = source
+  const lastvFeature = vsource
     .getFeatures()
-    .find((feature) => feature.get('last'));
-  flash(lastFeature);
+    .find((vfeature) => vfeature.get('last'));
+  const lastcFeature = csource
+    .getFeatures()
+    .find((cfeature) => cfeature.get('features').find((f) => f.get('last')));
+  if (lastcFeature) {
+    flash(lastcFeature);
+  } else {
+    flash(lastvFeature);
+  }
 };
 
 const loopRendering = () => {
   // at init only, eventually, add another layer to minimize the load.
-  vector.on('postrender', () => {
-    vector.changed();
+  vectorX.on('postrender', () => {
+    vectorX.changed();
   });
 };
 
-addRandomFeature();
-addRandomFeature();
-addRandomFeature();
-addRandomFeature();
+function addRandomFeature(last) {
+  const x = Math.random() * 360 - 180;
+  const y = Math.random() * 170 - 85;
+  const geom = new Point(fromLonLat([x, y]));
+  const feature = new Feature(geom);
+  if (last) {
+    feature.set('last', true);
+  }
+  vsource.addFeature(feature);
+}
+
+new Array(20).fill(0).forEach((_x) => addRandomFeature());
 addRandomFeature(true);
 doFlash();
 loopRendering();
+
+map.on('pointermove', (evt) => {
+  const map = evt.map;
+  map.forEachFeatureAtPixel(evt.pixel, (features) => {
+    console.log(
+      features.get('features').find((feature) => feature.get('last'))
+    );
+  });
+});
